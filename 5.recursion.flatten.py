625086e09457245de89cabae5d5cd992aad87de5
Fazil Abdul Lathif <fazillatheef@gmail.com>
2022-07-14 21:38:02 +0100

    flatten

---
+++ 5.recursion.flatten.py
@@ -0,0 +1,21 @@
+# flatten a multi dimension arrays into a 1 dimension array
+import unittest
+
+def flatten(l):
+    if l==[]:
+        return []
+    elif type(l)!= list:
+        return [l]
+    else:
+        result=[]
+        for e in l:
+            result += flatten(e)
+        return result 
+
+class test_flatten(unittest.TestCase):
+    def test_basic(self):
+        for i,o in [([1,2,3],[1,2,3]),([1,[2],3],[1,2,3]),([[1,2],3],[1,2,3]),([[]],[]),([],[]),([1,[],3],[1,3])]:
+            self.assertEqual(flatten(i),o,msg=f"For {i}, it should be {o}")
+
+if __name__ == '__main__':
+    unittest.main()
