c4f23871af5716d2f0e8d63f03a2c861545aa601
Fazil Abdul Lathif <fazillatheef@gmail.com>
2022-07-16 15:19:46 +0100

    First tabulation

--- 011.tab.word_break.py
+++ 011.tab.word_break.py
@@ -1,7 +1,16 @@
 # Check if a string can be made by just using the words in the provided dict
-
+# Table is bigger than the string by one byte. The end of a word is marked True. Then try other words
+# starting from the last found position. repeat this for all newly found word positions.
+# Then if the last position in the table is True it means the words can be used to create the string.
 def wordbreak(words,s):
-    pass
+    table = [False] * (len(s) + 1)
+    table[0] = True
+    for i in range(len(s)):
+        for j in range(i+1,len(s)+1):
+            if table[i] == True:
+                if s[i:j] in words:
+                    table[j]=True
+    return table[-1]
 
 print(wordbreak(['pen','apple'],"penapplepen")) # should be True
 print(wordbreak(['cat','and','dog'],"catandog")) # should be False
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Just practising coding
 Recursion is all about dynamically increasing loops and conditions. If recursion is done after a for loop. It is equivalent to having multiple nested for loops based on the conditions in the code.  
 
 Memoisation is a method to store results for most commonly re-used parts in a recursive code. We have to identify the thing that changes to store results in a dictionary(python).  
+
+Tabulation is using a parallel array to keep the intermediate results of an algorithm. It looks better than recursion. And feels more logical. Can be even implemented in COBOL
