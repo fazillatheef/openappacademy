340a8c6e1c0acd714c1cee20e5d77b4b51c1d698
Fazil Abdul Lathif <fazillatheef@gmail.com>
2022-07-16 14:53:38 +0100

    Word break

---
+++ 011.tab.word_break.py
@@ -0,0 +1,7 @@
+# Check if a string can be made by just using the words in the provided dict
+
+def wordbreak(words,s):
+    pass
+
+print(wordbreak(['pen','apple'],"penapplepen")) # should be True
+print(wordbreak(['cat','and','dog'],"catandog")) # should be False
