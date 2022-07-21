e05d461cfb65d87682f0cc401989b0fa8fa41125
Fazil Abdul Lathif <fazillatheef@gmail.com>
2022-07-21 17:37:51 +0100

    Tried another way to use dict for memoisation

--- 014.recursion.stepper.py
+++ 014.recursion.stepper.py
@@ -2,17 +2,25 @@
 # For the given array input. Make steps based on the numbers in the array
 # If you are able to reach the end then return True
 # :Recursion:
-def stepper(steps,memo={}):
+# Learned that default Arguments that are mutable retain values like static variable
+def stepper(steps,memo=None):
+    if memo == None:
+        memo = {}
+    key = len(steps)
+    if key in memo:
+        return memo[key]
     if len(steps)==0:
         return True
     for i in steps:
         if i==0:
             break
-        if stepper(steps[i:]):
+        if stepper(steps[i:],memo):
+            memo[key] = True
             return True
+    memo[key] = False
     return False
 
-print(stepper([1,2,1,0,1])) #False
+print(stepper([1,2,1,0,1]))#False
 print(stepper([1,2,1,1,1])) #True
 print(stepper([3,1,0,5,10])) #True
 print(stepper([3,4,0,1,10])) #True
